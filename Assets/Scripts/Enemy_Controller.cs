f463f6c0b39948e75bf46244a64f769da6eb4b9c
İrem Asena Kaplan <iremasenakaplan19@gmail.com>
2023-04-09 00:21:05 +0300

    .

--- Assets/Scripts/Enemy_Controller.cs
+++ Assets/Scripts/Enemy_Controller.cs
@@ -6,21 +6,25 @@ public class Enemy_Controller : MonoBehaviour
 {
    private Transform HousesPos;
    private Transform EnemyPos;
+   private Transform PlayerPos;
    public float speed;
    public float minDistance = 6f;
+   public GameObject FirePoint;
 
     void Start()
     {
         HousesPos = GameObject.FindGameObjectWithTag("Houses").GetComponent<Transform>(); //house in trans. erisilecek
         EnemyPos = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Transform>(); //enemy in trans. erisilecek
+        PlayerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); // player in trans. erisilecek
     }
 
     void Update()
     {
         float distance = Vector2.Distance(HousesPos.position, EnemyPos.position);
 
-        if(distance > minDistance){
-        transform.position = Vector2.MoveTowards(transform.position, HousesPos.position, speed * Time.deltaTime);
+        if(distance > minDistance)
+        {
+         transform.position = Vector2.MoveTowards(transform.position, HousesPos.position, speed * Time.deltaTime);
         }
        
     }
