ed0bc0b78d348c1d8f5ac927ca58874c1846176b
İrem Asena Kaplan <iremasenakaplan19@gmail.com>
2023-04-08 16:28:47 +0300

    menu level complated

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void GameStart() 
     {
-        
+        SceneManager.LoadScene("Scene2");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+       Application.Quit();
     }
 }
+
