9e482029fd7c2ece1a0ef4128b0a11bc81d27f6a
Tamer Molla <mollatamer@gmail.com>
2023-04-09 01:15:40 +0300

    tamerx

--- Assets/Scripts/enemyscript.cs
+++ Assets/Scripts/enemyscript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class enemyscript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public GameObject Player;
     void Start()
     {
         
@@ -13,6 +14,46 @@ public class enemyscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FindPlayerorHome();
+    }
+
+    void FindPlayerorHome()
+    {
+        float distanceclosestenemy = Mathf.Infinity;
+        Finding closestenemy = null;
+        Finding[] allenemies = GameObject.FindObjectsOfType<Finding>();
+
+        foreach (Finding currentenemy in allenemies)
+        {
+            float distancetoenemy = (currentenemy.transform.position - this.transform.position).sqrMagnitude;
+            if (distancetoenemy < distanceclosestenemy)
+            {
+                distanceclosestenemy = distancetoenemy;
+                closestenemy = currentenemy;
+
+
+            }
+        }
+
+        if ((Vector2.Distance(transform.position, closestenemy.transform.position) > 4)  && (closestenemy.name != "Player"))
+        {
+            
+            transform.position = Vector2.MoveTowards(transform.position, closestenemy.transform.position, speed * Time.deltaTime);
+           
+        }
+
+
+     else   if ((Vector2.Distance(transform.position, closestenemy.transform.position) > 1.35f) &&( closestenemy.name == "Player"))
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, closestenemy.transform.position, speed * Time.deltaTime);
+
+        }
+
+
+
+
+
+
     }
 }
